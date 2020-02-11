ca985f6a18d6a5c53a9d9ab275a87f03a45d5f11
Lukas <lukas.kogler@tuwien.ac.at>
2020-02-11 14:21:32 +0100

    cleaner block/no block

--- FlowTemplates.py
+++ FlowTemplates.py
@@ -34,14 +34,16 @@ class FlowOptions:
 ### MCS Discretization ###
 
 class MCS:
-    def __init__(self, settings = None, order = 2, RT = False, hodivfree = False, compress = True, truecompile = False):
+    def __init__(self, settings = None, order = 2, RT = False, hodivfree = False, compress = True, truecompile = False, pq_reg = 0):
 
+        self.settings = settings
         self.order = order
         self.RT = RT
         self.hodivfree = hodivfree
         self.compress = compress
         self.sym = settings.sym
         self.truecompile = truecompile
+        self.pq_reg = pq_reg
 
         # Spaces
         self.V = ngs.HDiv(settings.mesh, order = self.order, RT = self.RT, hodivfree = self.hodivfree, \
@@ -78,89 +80,110 @@ class MCS:
         else:
             self.X = ngs.FESpace([self.V, self.Vhat, self.Sigma])
 
-        dS = ngs.dx(element_vb = ngs.BND)
-        n = ngs.specialcf.normal(settings.mesh.dim)
-        def Compile (term):
-            return term.Compile(realcompile = self.truecompile, wait = True)
+    def Compile (self, term):
+        return term.Compile(realcompile = self.truecompile, wait = True)
+    
+    def SetUpForms(self, compound = False):
+
+        self.dS = ngs.dx(element_vb = ngs.BND)
+
+        n = ngs.specialcf.normal(self.settings.mesh.dim)
         def tang(u):
             return u - (u * n) * n
-        if settings.mesh.dim == 2:
+        if self.settings.mesh.dim == 2:
             def Skew2Vec(m):
                 return m[1, 0] - m[0, 1]
         else:
             def Skew2Vec(m):
                 return ngs.CoefficientFunction((m[0, 1] - m[1, 0], m[2, 0] - m[0, 2], m[1, 2] - m[2, 1]))
 
-        # # Forms - Block version
-        # if self.sym:
-        #     u, uhat, sigma, W = self.X.TrialFunction()
-        #     v, vhat, tau, R = self.X.TestFunction()
-        # else:
-        #     u, uhat, sigma = self.X.TrialFunction()
-        #     v, vhat, tau = self.X.TestFunction()
-        # p, q = self.Q.TnT()
-
-        # a_vol_cf = -1 / settings.nu * ngs.InnerProduct(sigma, tau) + ngs.div(sigma) * v + ngs.div(tau) * u
-        # if self.sym:
-        #     a_vol_cf += (ngs.InnerProduct(W, Skew2Vec(tau)) + ngs.InnerProduct(R, Skew2Vec(sigma)))
-        # a_bnd_cf = (-((sigma * n) * n) * (v * n) - ((tau * n) * n) * (u * n)) + \
-        #            (-(sigma * n) * tang(vhat) - (tau * n) * tang(uhat))
-        # b_vol_cf = ngs.div(u)*q
-        # bt_vol_cf = ngs.div(v)*p
-
-        # self.stokesA = Compile(a_vol_cf) * ngs.dx + Compile(a_bnd_cf) * dS
-        # self.stokesB = Compile(b_vol_cf) * ngs.dx
-        # self.stokesBT = Compile(bt_vol_cf) * ngs.dx
-
-        # if settings.vol_force is not None:
-        #     self.stokesf = settings.vol_force * v * ngs.dx
-        # else:
-        #     self.stokesf = None
-
-        # Forms - One big compound space
         if self.sym:
-            self.Xext = ngs.CompressCompound(ngs.FESpace([self.V, self.Vhat, self.Sigma, self.S, self.Q]))
-            u, uhat, sigma, W, p = self.Xext.TrialFunction()
-            v, vhat, tau, R, q = self.Xext.TestFunction()
+            self.X = ngs.FESpace([self.V, self.Vhat, self.Sigma, self.S])
         else:
-            self.Xext = ngs.CompressCompound(ngs.FESpace([self.V, self.Vhat, self.Sigma , self.Q]))
-            u, uhat, sigma, p = self.Xext.TrialFunction()
-            v, vhat, tau, q = self.Xext.TestFunction()
+            self.X = ngs.FESpace([self.V, self.Vhat, self.Sigma])
 
-        # self.Xext = ngs.FESpace( [self.X, self.Q] )
-        # if self.sym:
-        #     (u, uhat, sigma, W), p = self.Xext.TrialFunction()
-        #     (v, vhat, tau, R), q = self.Xext.TestFunction()
-        # else:
-        #     (u, uhat, sigma), p = self.Xext.TrialFunction()
-        #     (v, vhat, tau), q = self.Xext.TestFunction()
+        if compound:
+            self.Xext = ngs.FESpace([self.X, self.Q])
+            if self.sym:
+                (u, uhat, sigma, W), p = self.Xext.TrialFunction()
+                (v, vhat, tau, R), q = self.Xext.TestFunction()
+            else:
+                (u, uhat, sigma), p = self.Xext.TrialFunction()
+                (v, vhat, tau), q = self.Xext.TestFunction()
+        else:
+            self.Xext = None
+            if self.sym:
+                u, uhat, sigma, W = self.X.TrialFunction()
+                v, vhat, tau, R = self.X.TestFunction()
+            else:
+                u, uhat, sigma = self.X.TrialFunction()
+                v, vhat, tau = self.X.TestFunction()
+            p,q = self.Q.TnT()
 
-        print("Xext ranges :")
-        for k,x in enumerate(self.Xext.components):
-            print(self.Xext.Range(k).start, self.Xext.Range(k).stop)
-
-        aex_vol_cf = -1 / settings.nu * ngs.InnerProduct(sigma, tau) \
-                     + ngs.div(sigma) * v \
-                     + ngs.div(tau) * u
+        self.a_vol = -1 / self.settings.nu * ngs.InnerProduct(sigma, tau) \
+                        + ngs.div(sigma) * v \
+                        + ngs.div(tau) * u
         if self.sym:
-            aex_vol_cf += ngs.InnerProduct(W, Skew2Vec(tau)) \
+            self.a_vol += ngs.InnerProduct(W, Skew2Vec(tau)) \
                           + ngs.InnerProduct(R, Skew2Vec(sigma))
-        aex_bnd_cf = - ((sigma * n) * n) * (v * n) \
-                     - ((tau * n) * n) * (u * n) \
-                     - (sigma * n) * tang(vhat) \
-                     - (tau * n) * tang(uhat)
-        bex_vol_cf = ngs.div(u) * q
-        btex_vol_cf = ngs.div(v) * p
+        self.a_bnd = - ((sigma * n) * n) * (v * n) \
+                        - ((tau * n) * n) * (u * n) \
+                        - (sigma * n) * tang(vhat) \
+                        - (tau * n) * tang(uhat)
+        self.b_vol = ngs.div(u) * q
+        self.bt_vol = ngs.div(v) * p
 
-        aex_vol_cf += -1e-6 * settings.nu * p*q
+        if self.pq_reg != 0:
+            self.a_vol += -self.pq_reg * self.settings.nu * p*q
 
-        if settings.vol_force is not None:
-            self.stokesf = settings.vol_force * v * ngs.dx
+        if self.settings.vol_force is not None:
+            self.f_vol = self.settings.vol_force * v
         else:
-            self.stokesf = None
-
-        self.stokesM = Compile(aex_vol_cf + bex_vol_cf + btex_vol_cf) * ngs.dx + Compile(aex_bnd_cf) * dS
+            self.f_vol = None
             
+        self._stokesM = None
+        self._stokesA = None
+        self._stokesB = None
+        self._stokesBT = None
+        self._stokesBBT = None
+        self._stokesf = None
+
+
+    def stokesA(self):
+        if self._stokesA == None:
+            self._stokesA = self.Compile(self.a_vol) * ngs.dx + self.Compile(self.a_bnd) * self.dS
+        return self._stokesA
+
+    def stokesM(self):
+        if self._stokesM == None:
+            self._stokesM = self.Compile(self.a_vol + self.b_vol + self.bt_vol) * ngs.dx + self.Compile(self.a_bnd) * self.dS
+        return self._stokesM
+
+    def stokesB(self):
+        if self._stokesB == None:
+            self._stokesB = self.Compile(self.b_vol) * ngs.dx
+        return self._stokesB
+
+    def stokesBT(self):
+        if self._stokesB == None:
+            self._stokesB = self.Compile(self.bt_vol) * ngs.dx
+        return self._stokesB
+
+    def stokesBBT(self):
+        if self._stokesB == None:
+            self._stokesB = self.Compile(self.b_vol + self.bt_vol) * ngs.dx
+        return self._stokesB
+
+    def stokesM(self):
+        if self._stokesM == None:
+            self._stokesM = self.Compile(self.a_vol + self.b_vol + self.bt_vol) * ngs.dx + self.Compile(self.a_bnd) * self.dS
+        return self._stokesM
+
+    def stokesf(self):
+        if self._stokesf == None and self.f_vol is not None:
+            self._stokesf = self.Compile(self.f_vol) * ngs.dx
+        return self._stokesf
+
 ### END MCS Discretization ###
 
 
@@ -180,6 +203,8 @@ class StokesTemplate():
             if (self.block_la == True) and (pc_ver == "direct"):
                 raise "For direct solve, use block_la = False!"
 
+            stokes.disc.SetUpForms(compound = not self.block_la)
+            
             if self.block_la:
                 self.gfu = ngs.GridFunction(stokes.disc.X)
                 self.velocity = self.gfu.components[0]
@@ -189,8 +214,8 @@ class StokesTemplate():
                                                 self.pressure.vec])
             else:
                 self.gfu = ngs.GridFunction(stokes.disc.Xext)
-                self.velocity = self.gfu.components[0]
-                self.pressure = self.gfu.components[-1]
+                self.velocity = self.gfu.components[0].components[0]
+                self.pressure = self.gfu.components[1]
                 self.sol_vec = self.gfu.vec
             
             self._to_assemble = []
@@ -232,16 +257,16 @@ class StokesTemplate():
             # Forward operators
             if self.block_la:
                 self.a = ngs.BilinearForm(stokes.disc.X, eliminate_internal = self.elint, eliminate_hidden = True)
-                self.a += stokes.disc.stokesA
+                self.a += stokes.disc.stokesA()
 
                 self.b = ngs.BilinearForm(trialspace = stokes.disc.X, testspace = stokes.disc.Q)
-                self.b += stokes.disc.stokesB
+                self.b += stokes.disc.stokesB()
 
-                self._to_assemble += [self.a, self.b]
+                self._to_assemble += [ self.a, self.b ]
 
             else:
                 self.m = ngs.BilinearForm(stokes.disc.Xext, eliminate_internal = self.elint, eliminate_hidden = stokes.disc.compress)
-                self.m += stokes.disc.stokesM
+                self.m += stokes.disc.stokesM()
 
                 self._to_assemble += [ self.m ]
                 
@@ -249,8 +274,8 @@ class StokesTemplate():
             # Right hand side
             if self.block_la:
                 self.f = ngs.LinearForm(stokes.disc.X)
-                if stokes.disc.stokesf is not None:
-                    self.f += stokes.disc.stokesf
+                if stokes.disc.stokesf() is not None:
+                    self.f += stokes.disc.stokesf()
 
                 self.g = ngs.LinearForm(stokes.disc.Q)
 
@@ -258,8 +283,8 @@ class StokesTemplate():
 
             else:
                 self.f = ngs.LinearForm(stokes.disc.Xext)
-                if stokes.disc.stokesf is not None:
-                    self.f += stokes.disc.stokesf
+                if stokes.disc.stokesf() is not None:
+                    self.f += stokes.disc.stokesf()
 
                 self._to_assemble += [ self.f ]
                 
@@ -268,7 +293,6 @@ class StokesTemplate():
             if self.block_la:
                 raise "Cannot invert block matrices!"
             else:
-                print("MAT, ", self.m.mat)
                 itype = "umfpack" if inv_type is None else inv_type
                 self.Mpre = self.M.Inverse(stokes.disc.Xext.FreeDofs(self.elint), inverse = itype)
             
